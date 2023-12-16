fda5be05a4beb0a784dc58bcd406f1be9eaf7d5c
Willem <91901507+wverbuyst@users.noreply.github.com>
2023-12-16 17:52:15 +0100

    Add dummy visitors with faker

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,7 @@
 import fp from "fastify-plugin";
 import { Low } from "lowdb";
 import { JSONFile } from "lowdb/node";
+import { faker } from "@faker-js/faker";
 
 export interface DbSchema {
   visitors: string[];
@@ -9,11 +10,13 @@ export interface DbSchema {
 interface LowDbPluginOptions {}
 
 export default fp<LowDbPluginOptions>(async (server) => {
+  const visitors = [...Array(20).keys()].map(() => faker.person.firstName());
+
   const adapter = new JSONFile<DbSchema>("db.json");
-  const db = new Low<DbSchema>(adapter, { visitors: [] });
+  const db = new Low<DbSchema>(adapter, { visitors });
 
   await db.read();
-  db.data ||= { visitors: [] }; // Initialize data if empty
+  db.data ||= { visitors }; // Initialize data if empty
   await db.write();
 
   server.decorate("db", db);
