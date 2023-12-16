e2278f8961844c2dcc106155cabbcd25a7f3544e
Willem Verbuyst <willemverbuyst@gmail.com>
2023-12-16 09:03:13 +0100

    Use pino for logging

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,12 @@ import formbody from "@fastify/formbody";
 import fastifyView from "@fastify/view";
 import ejs from "ejs";
 import fastify from "fastify";
+import pino from "pino";
+import pretty from "pino-pretty";
 import routes from "./routes";
 
 export const server = fastify({
-  logger: true,
+  logger: pino(pretty()),
 });
 
 server.register(fastifyView, {
